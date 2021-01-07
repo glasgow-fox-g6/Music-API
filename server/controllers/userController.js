06c9d2f3f11efe37ee5e4495d5d39951701d080c
cakra bilisairo <cakra.bilisairo01@gmail.com>
2021-01-07 18:54:50 +0700

    Starter pack

---
+++ server/controllers/userController.js
@@ -0,0 +1,12 @@
+const {User} = require('../models')
+
+class Controller{
+    static register (req, res, next){
+
+    }
+    static login (req, res, next){
+
+    }
+}
+
+module.exports = Controller
