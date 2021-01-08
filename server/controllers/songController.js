f199bcafb9b154f5811aa6c9ef646202356e464a
Aldyco Turang <aldycoturang@gmail.com>
2021-01-08 10:11:01 +0700

    Server songs search API

--- server/controllers/songController.js
+++ server/controllers/songController.js
@@ -1,17 +1,56 @@
-const {Song} = require('../models')
+const axios = require('axios')
 
-class Controller{
-    static listSong(req, res, next){
-        res.send('testing in')
-    }
-    static addSong(req, res, next){
+const { Song } = require('../models')
 
-    }
-    static getSongById(req, res, next){
-        
-    }
-    static searchSong(req, res, next){
-        
-    }
+const iTunesAPI = 'https://itunes.apple.com/search'
+
+class Controller {
+
+  static listSong(req, res, next) {
+    res.send('testing in')
+  }
+
+  static addSong(req, res, next) {
+
+  }
+
+  static getSongById(req, res, next) {
+
+  }
+
+  static searchSong(req, res, next) {
+    axios.get(iTunesAPI, {
+      params: {
+        term: req.query.q,
+        // limit: 20,
+        media: 'music'
+      }
+    })
+      .then(response => {
+        let songs = response.data.results.filter(data => data.kind === 'song')
+        let results = songs.map(song => {
+          return {
+            title: song.trackName,
+            artist: song.artistName,
+            album: song.collectionName,
+            genre: song.primaryGenreName,
+            releaseDate: song.releaseDate,
+            trackId: song.trackId,
+            appleStoreUrl: song.trackViewUrl,
+            previewUrl: song.previewUrl,
+            artworkUrl: song.artworkUrl100,
+            trackTimeMillis: song.trackTimeMillis
+          }
+        })
+        if(results.length > 0) {
+          res.status(200).json(results)
+        } else {
+          next({ name: 'NotFound' })
+        }
+      })
+      .catch(error => {
+        next(error)
+      })
+  }
 }
 module.exports = Controller
