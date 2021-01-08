432f59d4d7c77d4d1e8d7c474a5eebbfd5e9105b
Aldyco Turang <aldycoturang@gmail.com>
2021-01-08 13:02:30 +0700

    bugfix and update songs router

--- server/routes/songRouter.js
+++ server/routes/songRouter.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const Controller = require('../controllers/songController')
 
-router.get('/songs', Controller.listSong)
-router.post('/songs', Controller.addSong)
-router.get('/songs/:id', Controller.getSongById)
-router.get('/songs/search', Controller.searchSong)
+router.get('/', Controller.listSong)
+router.post('/', Controller.addSong)
+router.get('/search', Controller.searchSong)
+router.get('/:id', Controller.getSongById)
 
 module.exports = router
